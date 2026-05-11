c5f8f72e00c1501cc253e7c837871e8dec24ca69
wvmichael <michael@weathervalet.com>
2026-05-11 19:48:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2946,3 +2946,18 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     debug = os.environ.get("FLASK_DEBUG") == "1"
     app.run(host="0.0.0.0", port=port, debug=debug)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
